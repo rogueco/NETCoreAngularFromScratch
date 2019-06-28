d85236fe2d5c4b4d21bd892173939676862855e6
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-06-28 14:46:23 +0100

    Error handling added.

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -49,8 +49,6 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
 
-            throw new Exception("computer says no");
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
