121e59e20fd3885366a1af0b7afcbd95810c68b3
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-08 04:42:11 +0100

    Messages components added.

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
 import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
    declarations: [
@@ -23,7 +24,8 @@ import { ListsComponent } from './lists/lists.component';
       HomeComponent,
       RegisterComponent,
       MemberListComponent,
-      ListsComponent
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
