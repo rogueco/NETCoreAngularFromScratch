3300e8adfd69c52e4c51ecd67d11f8d6a695aed3
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-08 04:41:42 +0100

    Added List Component.

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
 import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
 
 @NgModule({
    declarations: [
@@ -21,7 +22,8 @@ import { MemberListComponent } from './member-list/member-list.component';
       NavComponent,
       HomeComponent,
       RegisterComponent,
-      MemberListComponent
+      MemberListComponent,
+      ListsComponent
    ],
    imports: [
       BrowserModule,
