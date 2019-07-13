6e9f89cf427800579ecc8635a3cf87fc49ac60fb
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-13 14:43:11 +0100

    Ability for photo creating shizniz

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -39,6 +39,17 @@ namespace DatingApp.API.Controllers
             _cloudinary = new Cloudinary(acc);
         }
 
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult> GetPhoto(int id)
+        {
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto)
         {
@@ -80,10 +91,11 @@ namespace DatingApp.API.Controllers
 
             if (await _repo.SaveAll())
             {
-                return Ok();
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new { id = photo.Id }, photoToReturn);
             }
 
-
+            return BadRequest("Could not add the photo");
         }
     }
 }
