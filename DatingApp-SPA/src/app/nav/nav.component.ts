abb7be0d5d372002d3273b9c0817674ca4869136
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-14 15:23:12 +0100

    fixed format

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "../_services/auth.service";
-import { AlertifyService } from "../_services/alertify.service";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-nav",
-  templateUrl: "./nav.component.html",
-  styleUrls: ["./nav.component.css"]
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
@@ -22,13 +22,13 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model).subscribe(
       next => {
-        this.alertify.success("Logged in successfully");
+        this.alertify.success('Logged in successfully');
       },
       error => {
         this.alertify.error(error);
       },
       () => {
-        this.router.navigate(["/members"]);
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -38,11 +38,11 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
     this.authService.decodedToken = null;
     this.authService.currentUser = null;
-    this.alertify.message("logged out");
-    this.router.navigate([""]);
+    this.alertify.message('logged out');
+    this.router.navigate(['']);
   }
 }
