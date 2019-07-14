b19a08d0e5adf967ef64127244690d8a4a2c641c
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-14 15:34:29 +0100

    Image now persists on a hard refresh.

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -76,6 +76,11 @@ export class PhotoEditorComponent implements OnInit {
           this.currentMain.isMain = false;
           photo.isMain = true;
           this.authService.changeMemberPhoto(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem(
+            'user',
+            JSON.stringify(this.authService.currentUser)
+          );
         },
         error => {
           this.alertify.error(error);
