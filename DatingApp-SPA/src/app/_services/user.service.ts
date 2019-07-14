5bcaa83599fa687c00d394de012ebd198d5793a2
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-14 16:08:54 +0100

    Ability to delete photo from the SPA

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   baseUrl = environment.apiUrl;
@@ -24,7 +24,14 @@ export class UserService {
     return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 
-  setMainPhoto(userId: number, id: number){
-    return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain',{});
+  setMainPhoto(userId: number, id: number) {
+    return this.http.post(
+      this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain',
+      {}
+    );
+  }
+
+  deletePhoto(userId: number, id: number) {
+    return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
   }
 }
