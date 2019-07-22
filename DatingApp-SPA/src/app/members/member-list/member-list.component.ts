28df3038b3992eaca114b1e55b58c6535f9e6c61
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-22 12:21:18 +0100

    Added click events on filters in spa.

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -34,6 +34,7 @@ export class MemberListComponent implements OnInit {
     this.userParams.gender = this.user.gender = 'female' ? 'male' : 'female';
     this.userParams.minAge = 18;
     this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
   }
 
   pageChanged(event: any): void {
@@ -50,7 +51,11 @@ export class MemberListComponent implements OnInit {
 
   loadUsers() {
     this.userService
-      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+      .getUsers(
+        this.pagination.currentPage,
+        this.pagination.itemsPerPage,
+        this.userParams
+      )
       .subscribe(
         (res: PaginatedResult<User[]>) => {
           this.users = res.result;
