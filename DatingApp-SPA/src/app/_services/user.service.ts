85224609485ca76a3563c9ada24e61c128e97ae4
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-23 12:58:40 +0100

    Added a who likes who page...

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -17,7 +17,8 @@ export class UserService {
   getUsers(
     page?,
     itemsPerPage?,
-    userParams?
+    userParams?,
+    likesParam?
   ): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
       User[]
@@ -35,6 +36,14 @@ export class UserService {
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
+    }
+
+    if (likesParam == 'Likers') {
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParam == 'Likees') {
+      params = params.append('likees', 'true');
     }
 
     return this.http
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -10,6 +10,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,22 +22,26 @@ export const appRoutes: Routes = [
       {
         path: 'members',
         component: MemberListComponent,
-        resolve: { users: MemberListResolver }
+        resolve: { users: MemberListResolver },
       },
       {
         path: 'members/:id',
         component: MemberDetailComponent,
-        resolve: { user: MemberDetailResolver }
+        resolve: { user: MemberDetailResolver },
       },
       { path: 'messages', component: MessagesComponent },
       {
         path: 'member/edit',
         component: MemberEditComponent,
         resolve: { user: MemberEditResolver },
-        canDeactivate: [PreventUnsavedChanges]
+        canDeactivate: [PreventUnsavedChanges],
       },
-      { path: 'lists', component: ListsComponent }
-    ]
+      {
+        path: 'lists',
+        component: ListsComponent,
+        resolve: { users: ListsResolver },
+      },
+    ],
   },
-  { path: '**', redirectTo: ' home', pathMatch: 'full' }
+  { path: '**', redirectTo: ' home', pathMatch: 'full' },
 ];
