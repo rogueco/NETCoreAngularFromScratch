c3358c3befbff27aee4850ff330ff40c4adf038c
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2019-07-24 11:17:08 +0100

    Send messages in the SPA. Fixed a few issues. BLAH

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -125,4 +125,8 @@ export class UserService {
       this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId
     );
   }
+
+  sendMessage(id: number, message: Message) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/messages', message);
+  }
 }
--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -13,6 +13,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 export class MemberMessagesComponent implements OnInit {
   @Input() recipientId: number;
   messages: Message[];
+  newMessage: any = {};
 
   constructor(
     private userService: UserService,
@@ -31,6 +32,21 @@ export class MemberMessagesComponent implements OnInit {
       .subscribe(
         messages => {
           this.messages = messages;
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
+  }
+
+  sendMessage() {
+    this.newMessage.recipientId = this.recipientId;
+    this.userService
+      .sendMessage(this.authService.decodedToken.nameid, this.newMessage)
+      .subscribe(
+        (message: Message) => {
+          this.messages.unshift(message);
+          this.newMessage.content = '';
         },
         error => {
           this.alertify.error(error);
